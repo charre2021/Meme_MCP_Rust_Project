0d3ce903309f8beb2e8917d83d77ca6f3364ca56
charre2021 <charre2021@outlook.com>
2026-02-15 11:37:41 -0600

    adding in keyword search functionality through clap

--- meme/src/main.rs
+++ meme/src/main.rs
@@ -1,8 +1,16 @@
+use clap::Parser;
 use dotenv::dotenv;
 use serde::{Deserialize, Serialize};
 use std::env;
 
 const BASE_URL: &str = "https://api.apileague.com/retrieve-random-meme";
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    keyword_to_search: String,
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Meme {
@@ -21,10 +29,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let Ok(api_key) = env::var("API_LEAGUE_API_KEY") else {
         panic!("API key could not be loaded correctly from the environmental variables.");
     };
+    let arg = Args::parse();
     let params = [
-        ("keywords", "cat"),
+        ("keywords", &arg.keyword_to_search),
         ("api-key", &api_key),
-        ("media-type", "image/jpeg"),
+        ("media-type", &String::from("image/jpeg")),
     ];
     let client = reqwest::Client::new();
     let content: Meme = client
