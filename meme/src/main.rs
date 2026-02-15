ee39a3b5a66ba61f27041036fd6185a6ed9980e4
charre2021 <charre2021@outlook.com>
2026-02-15 11:15:27 -0600

    adding serde

--- meme/src/main.rs
+++ meme/src/main.rs
@@ -1,7 +1,19 @@
 use dotenv::dotenv;
+use serde::{Deserialize, Serialize};
 use std::env;
 
-const BASE_URL : &str = "https://api.apileague.com/retrieve-random-meme";
+const BASE_URL: &str = "https://api.apileague.com/retrieve-random-meme";
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Meme {
+    description: String,
+    url: String,
+    #[serde(alias = "type")]
+    meme_type: String,
+    width: u16,
+    height: u16,
+    ratio: f32,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -9,11 +21,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let Ok(api_key) = env::var("API_LEAGUE_API_KEY") else {
         panic!("API key could not be loaded correctly from the environmental variables.");
     };
-    let params = [("keywords", "cat"), ("api-key", &api_key), ("media-type", "image/jpeg")];
+    let params = [
+        ("keywords", "cat"),
+        ("api-key", &api_key),
+        ("media-type", "image/jpeg"),
+    ];
     let client = reqwest::Client::new();
-    let content = client.get(BASE_URL).query(&params).send().await?.text().await?;
-        
-    println!("Here is the response: {content:?}");
-    Ok(())
+    let content: Meme = client
+        .get(BASE_URL)
+        .query(&params)
+        .send()
+        .await?
+        .json()
+        .await?;
 
+    println!("Here is your content: {content:?}");
+
+    Ok(())
 }
